24c5bfd7a08ab0693b4da62937e22c983ec257be
AliyanAftab0 <aliyanaftab2009.com>
2024-08-31 20:17:29 +0500

    added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,9 +2,7 @@
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import Instagram from "../../public/Instagram.png";
-import Github from "../../public/Github.png";
-import Linkedin from "../../public/Linkedin.png";
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 import { useForm, ValidationError } from "@formspree/react";
 
 const Contact = () => {
@@ -15,140 +13,139 @@ const Contact = () => {
   }
 
   return (
-  <>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=New+Amsterdam&display=swap"
         rel="stylesheet"
       />
-    <section className="text-gray-600 sm:mx-40 Aliyan-Dev body-font relative">
-      <div className="container px-5 py-3 mx-auto flex flex-wrap">
-        <div className="mx-12 my-10 dark:text-white">
-          <div className="flex flex-col gap-[1.5rem]">
-            <div>
-              <p className="text-blue-600 text-xl">Contact Us</p>
-              <h1 className="text-4xl">
-                Have a <span className="text-blue-600">Project</span> or want to{" "}
-                <br />
-                <span className="text-blue-600">Collaborate</span>?
-              </h1>
-              <p className="text-xl">
-                <span>We would love to hear from you.</span>
-                <span>Drop a message.</span>
-              </p>
-            </div>
-            <div className="border-2 rounded-md dark:bg-slate-900 shadow-2xl p-6 dark:shadow-slate-800 shadow-slate-400">
+      <section className="text-gray-600 sm:mx-40 Aliyan-Dev body-font relative">
+        <div className="container px-5 py-3 mx-auto flex flex-wrap">
+          <div className="mx-12 my-10 dark:text-white">
+            <div className="flex flex-col gap-[1.5rem]">
               <div>
-                <p className="text-xl">Location</p>
-                <p className="text-xl">Karachi, Sindh</p>
+                <p className="text-blue-600 text-xl">Contact Us</p>
+                <h1 className="text-4xl">
+                  Have a <span className="text-blue-600">Project</span> or want
+                  to <br />
+                  <span className="text-blue-600">Collaborate</span>?
+                </h1>
+                <p className="text-xl">
+                  <span>We would love to hear from you.</span>
+                  <span>Drop a message.</span>
+                </p>
               </div>
-              <div>
-                <p className="text-xl">Mail at</p>
-                <p className="text-xl">aliyandev0@gmail.com</p>
-              </div>
-              <div>
-                <p className="text-xl">Follow on</p>
-                <span className="flex my-3 gap-5 cursor-pointer">
-                  <Link href="https://www.instagram.com/aliyandev/">
-                    <Image
-                      src={Instagram}
-                      alt="Instagram"
-                      width={32}
-                      height={32}
-                    />
-                  </Link>
-                  <Link href="https://github.com/AliyanAftab0">
-                    <Image src={Github} alt="Github" width={32} height={32} />
-                  </Link>
-                  <Link href="https://www.linkedin.com/in/aliyan-aftab-b95b962b4/">
-                    <Image
-                      src={Linkedin}
-                      alt="Linkedin"
-                      width={32}
-                      height={32}
-                    />
-                  </Link>
-                </span>
+              <div className="border-2 text-center rounded-md dark:bg-slate-900 shadow-2xl p-6 dark:shadow-slate-800 shadow-slate-400">
+                <div className="border-2 mb-3 p-3 bg-slate-100 dark:bg-slate-800 rounded-md">
+                  <p className="text-xl">Location</p>
+                  <p className="text-xl">Karachi, Sindh</p>
+                </div>
+                <div className="border-2 mb-3 p-3 bg-slate-100 dark:bg-slate-800 rounded-md">
+                  <p className="text-xl">Mail at</p>
+                  <p className="text-xl text-blue-500">aliyandev0@gmail.com</p>
+                </div>
+                <div className="border-2 mb-3 p-3 bg-slate-100 dark:bg-slate-800 rounded-md">
+                  <p className="text-xl">Follow on</p>
+                  <span className="flex my-3 gap-5 items-center justify-center cursor-pointer">
+                    <Link
+                      href="https://www.instagram.com/aliyandev/"
+                      className="text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400"
+                    >
+                      <FaInstagram size={30} />
+                    </Link>
+                    <Link
+                      href="https://www.linkedin.com/in/aliyan-aftab-b95b962b4/"
+                      className="text-gray-700 dark:text-gray-300 hover:text-blue-700 dark:hover:text-blue-500"
+                    >
+                      <FaLinkedin size={30} />
+                    </Link>
+                    <Link
+                      href="https://github.com/AliyanAftab0"
+                      className="text-gray-700 dark:text-gray-300 hover:text-blue-700 dark:hover:text-blue-500"
+                    >
+                      <FaGithub size={30} />
+                    </Link>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
+          <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 mb-6 border-2 rounded-md px-3 dark:shadow-slate-800 dark:bg-slate-900 shadow-2xl shadow-slate-400">
+            <h2 className="text-gray-900 dark:text-white text-lg mb-1 font-medium title-font">
+              Get In Touch
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600 dark:text-white">
+              Have questions, feedback, or want to collaborate? Drop a message
+              below!
+            </p>
+            <form onSubmit={handleSubmit}>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="name"
+                  className="leading-7 text-sm text-gray-600 dark:text-white"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="w-full bg-gray-100 rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600 dark:text-white"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="w-full bg-gray-100 rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="message"
+                  className="leading-7 text-sm text-gray-600 dark:text-white"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="w-full bg-gray-100 rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                />
+              </div>
+              <Button
+                type="submit"
+                className={buttonVariants({ variant: "default" })}
+                disabled={state.submitting}
+              >
+                Send Message
+              </Button>
+            </form>
+            <p className="text-xs text-gray-500 dark:text-white mt-3">
+              Your privacy is important to us. We won't share your details with
+              anyone.
+            </p>
+          </div>
         </div>
-        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0 mb-6 border-2 rounded-md px-3 dark:shadow-slate-800 dark:bg-slate-900 shadow-2xl shadow-slate-400">
-          <h2 className="text-gray-900 dark:text-white text-lg mb-1 font-medium title-font">
-            Get In Touch
-          </h2>
-          <p className="leading-relaxed mb-5 text-gray-600 dark:text-white">
-            Have questions, feedback, or want to collaborate? Drop a message
-            below!
-          </p>
-          <form onSubmit={handleSubmit}>
-            <div className="relative mb-4">
-              <label
-                htmlFor="name"
-                className="leading-7 text-sm text-gray-600 dark:text-white"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full bg-gray-100 rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="email"
-                className="leading-7 text-sm text-gray-600 dark:text-white"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-gray-100 rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-              <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-              />
-            </div>
-            <div className="relative mb-4">
-              <label
-                htmlFor="message"
-                className="leading-7 text-sm text-gray-600 dark:text-white"
-              >
-                Message
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-gray-100 rounded border border-gray-300 focus:border-gray-500 focus:ring-2 focus:ring-gray-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              ></textarea>
-              <ValidationError
-                prefix="Message"
-                field="message"
-                errors={state.errors}
-              />
-            </div>
-            <Button
-              type="submit"
-              className={buttonVariants({ variant: "default" })}
-              disabled={state.submitting}
-            >
-              Send Message
-            </Button>
-          </form>
-          <p className="text-xs text-gray-500 dark:text-white mt-3">
-            Your privacy is important to us. We won't share your details with
-            anyone.
-          </p>
-        </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -20,86 +20,83 @@ export default function Home() {
 
   return (
     <>
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=New+Amsterdam&display=swap"
         rel="stylesheet"
       />
-    <main>
-      <section className="container px-4 py-10 Aliyan-Dev mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
-        <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
-          <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
-            <span className="font-bold Aliyan-Dev text-[3rem]">
-              <h1>
+      <main>
+        <section className="container px-4 py-10 Aliyan-Dev mx-auto lg:h-128 lg:space-x-8 lg:flex lg:items-center">
+          <div className="w-full text-center lg:text-left lg:w-1/2 lg:-mt-8">
+            <h1 className="text-3xl leading-snug text-gray-800 dark:text-gray-200 md:text-4xl">
+              <span className="font-bold Aliyan-Dev text-[3rem]">
                 Welcome To <span className="text-blue-600">&lt;/&gt;</span>
-                Aliyan
-              </h1>
-              <span className="text-blue-600">Devs</span>
-            </span>
-            <br />
-            <span className="font-bold Aliyan-Dev border-b-4 border-blue-600 decoration-primary">
-              <span ref={el} />
-            </span>
-          </h1>
-          <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
-            Passionate software developer with over 1.5 years of experience in
-            creating innovative solutions. Skilled in various programming
-            languages and frameworks, I specialize in building robust and
-            scalable applications. Explore my projects and see how I can bring
-            your ideas to life!
-          </p>
-        </div>
-        <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
-          <Image
-            src="https://www.creative-tim.com/twcomponents/svg/website-designer-bro-purple.svg"
-            alt="tailwind css components"
-            width={500}
-            height={500}
-            className="w-full h-full max-w-md mx-auto"
-          />
-        </div>
-      </section>
-
-      <section className="text-gray-600 Aliyan-Dev text-xl dark:text-gray-50 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <h2 className="text-4xl pb-8 mb-4 font-bold text-center">
-            About Our Services
-          </h2>
-          <div className="flex flex-wrap justify-center -m-4">
-            <div className="p-4 lg:w-1/3">
-              <div className="h-full bg-gray-200 dark:bg-gray-900 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg">
-                <h2 className="tracking-widest title-font font-medium mb-1">
-                  WEB DEVELOPMENT
-                </h2>
-                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 dark:text-white mb-3">
-                  Full-Stack Solutions
-                </h1>
-                <p className="leading-relaxed mb-3 text-gray-700 dark:text-gray-50">
-                  Building robust, scalable, and responsive web applications
-                  using modern tech stacks like React, Node.js, and more.
-                </p>
-                <a className="inline-flex items-center hover:text-indigo-700">
-                  Learn More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                Aliyan<span className="text-blue-600">Devs</span>
+              </span>
+              <br />
+              <span className="font-bold Aliyan-Dev border-b-4 border-blue-600 decoration-primary">
+                <span ref={el} />
+              </span>
+            </h1>
+            <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
+              Passionate software developer with over 1.5 years of experience in
+              creating innovative solutions. Skilled in various programming
+              languages and frameworks, I specialize in building robust and
+              scalable applications. Explore my projects and see how I can bring
+              your ideas to life!
+            </p>
+          </div>
+          <div className="w-full mt-4 lg:mt-0 lg:w-1/2">
+            <Image
+              src="https://www.creative-tim.com/twcomponents/svg/website-designer-bro-purple.svg"
+              alt="tailwind css components"
+              width={500}
+              height={500}
+              className="w-full h-full max-w-md mx-auto"
+            />
+          </div>
+        </section>
+        <div className="border-t border-gray-300 mx-10 dark:border-gray-700 mt-8 pt-4 text-center"></div>
+        <section className="text-gray-600 Aliyan-Dev text-xl dark:text-gray-50 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <h2 className="text-4xl pb-8 mb-4 font-bold text-center">
+              About Our Services
+            </h2>
+            <div className="flex flex-wrap justify-center -m-4">
+              <div className="p-4 lg:w-1/3">
+                <div className="h-full bg-gray-200 dark:bg-gray-900 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative shadow-lg">
+                  <h2 className="tracking-widest title-font font-medium mb-1">
+                    WEB DEVELOPMENT
+                  </h2>
+                  <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 dark:text-white mb-3">
+                    Full-Stack Solutions
+                  </h1>
+                  <p className="leading-relaxed mb-3 text-gray-700 dark:text-gray-50">
+                    Building robust, scalable, and responsive web applications
+                    using modern tech stacks like React, Node.js, and more.
+                  </p>
+                  <a className="inline-flex items-center hover:text-indigo-700">
+                    Learn More
+                    <svg
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
     </>
   );
 }
