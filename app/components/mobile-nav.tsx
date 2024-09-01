0e875b11bbd0daacc37ca8afa15c5565ca09a87e
AliyanAftab0 <aliyanaftab2009.com>
2024-09-01 14:08:13 +0500

    added

--- app/components/mobile-nav.tsx
+++ app/components/mobile-nav.tsx
@@ -40,37 +40,45 @@ const MobileNav = () => {
       <ul className="text-md Aliyan-Dev">
         <li
           className={`py-4 ${
-            activeSection === "home" ? "border-b-2 border-blue-600" : ""
-          }`}
+            activeSection === "home" ? "border-b-4 border-blue-600" : ""
+          } transition-all duration-300 ease-in-out`}
           onClick={() => handleLinkClick("home")}
         >
-          <Link href={"/"}>Home</Link>
+          <Link href={"/"} className="hover:text-blue-600">
+            Home
+          </Link>
         </li>
         <li
           className={`py-4 ${
-            activeSection === "about" ? "border-b-2 border-blue-600" : ""
-          }`}
+            activeSection === "about" ? "border-b-4 border-blue-600" : ""
+          } transition-all duration-300 ease-in-out`}
           onClick={() => handleLinkClick("about")}
         >
-          <Link href={"/about"}>About</Link>
+          <Link href={"/about"} className="hover:text-blue-600">
+            About
+          </Link>
         </li>
         <li
           className={`py-4 ${
-            activeSection === "projects" ? "border-b-2 border-blue-600" : ""
-          }`}
+            activeSection === "projects" ? "border-b-4 border-blue-600" : ""
+          } transition-all duration-300 ease-in-out`}
           onClick={() => handleLinkClick("projects")}
         >
-          <Link href={"/projects"}>Projects</Link>
+          <Link href={"/projects"} className="hover:text-blue-600">
+            Projects
+          </Link>
         </li>
         <li
           className={`py-4 ${
-            activeSection === "contact" ? "border-b-2 border-blue-600" : ""
-          }`}
+            activeSection === "contact" ? "border-b-4 border-blue-600" : ""
+          } transition-all duration-300 ease-in-out`}
           onClick={() => handleLinkClick("contact")}
         >
-          <Link href={"/contact"}>Contact</Link>
+          <Link href={"/contact"} className="hover:text-blue-600">
+            Contact
+          </Link>
         </li>
-      </ul> 
+      </ul>
     </div>
   );
 };
